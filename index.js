4feec444185c317daa6c27736cb65781f5ff7881
AChristoff <aleksov101@gmail.com>
2020-02-29 11:57:24 +0200

    add handlers

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const url = require('url');
 const port = 3000;
+const handles = require('./handlers/index')
 
 http.createServer((req, res) => {
     res.write('Server is working');
     res.end();
 }).listen(port);
+ 
