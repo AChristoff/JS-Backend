0189ac3dc2784abb6459d00aa8ea05010300d3d3
AChristoff <aleksov101@gmail.com>
2020-05-26 22:16:35 +0300

    user crud

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,14 @@
 const restrictedPages = require('./auth');
 const homeController = require('../controllers/home');
+const userController = require('../controllers/user');
 
 module.exports = app => {
     app.get('/', homeController.index);
+    app.get('/user/register', restrictedPages.isAnonymous, userController.registerGet);
+    app.post('/user/register', restrictedPages.isAnonymous, userController.registerPost);
+    app.get('/user/login', restrictedPages.isAnonymous, userController.loginGet);
+    app.post('/user/login', restrictedPages.isAnonymous, userController.loginPost);
+    app.post('/user/logout', userController.logout);
 
     app.all('*', (req, res) => {
         res.status(404);
--- controllers/user.js
+++ controllers/user.js
@@ -1,17 +1,88 @@
+const encryption = require('../util/encryption');
+const User = require('../models/User');
+
 module.exports = {
     registerGet: (req, res) => {
-        // TODO:
+        res.render('user/register');
     },
     registerPost: async (req, res) => {
-       // TODO:
+        const userBody = req.body;
+
+        if (!userBody.username || !userBody.password || !userBody.repeatPassword) {
+            userBody.error = 'Please fill all fields!';
+            res.render('user/register', userBody);
+            return;
+        }
+
+        if (userBody.password !== userBody.repeatPassword) {
+            userBody.error = 'Passwords do not match!';
+            res.render('user/register', userBody);
+            return;
+        }
+
+        const salt = encryption.generateSalt();
+        const hashedPass = encryption.generateHashedPassword(salt, userBody.password);
+
+        try {
+            const user = await User.create({
+                username: userBody.username,
+                hashedPass,
+                salt,
+                firstName: userBody.firstName,
+                lastName: userBody.lastName,
+                roles: ['User']
+            });
+
+            req.logIn(user, (err) => {
+                if (err) {
+                    userBody.error = err;
+                    res.render('user/register', userBody);
+                } else {
+                    res.redirect('/');
+                }
+            });
+
+        } catch (err) {
+            console.log(err);
+        }
+
     },
     logout: (req, res) => {
-        // TODO:
+        req.logout();
+        res.redirect('/');
     },
     loginGet: (req, res) => {
-        // TODO:
+        res.render('user/login');
     },
     loginPost: async (req, res) => {
-        // TODO:
+        const userBody = req.body;
+
+        try {
+            const user = await User.findOne({username: userBody.username});
+            if (!user) {
+                userBody.error = 'Username is invalid!';
+                res.render('user/login');
+                return;
+            }
+
+            if (!user.authenticate(userBody.password)) {
+                userBody.error = 'Password id invalid';
+                res.render('user/login', userBody);
+            }
+
+            req.logIn(user, (err) => {
+                if (err) {
+                    userBody.error = err;
+                    res.render('user/login', userBody);
+                } else {
+                    res.redirect('/');
+                }
+            });
+
+
+        } catch (err) {
+            userBody.error = err;
+            res.render('user/login', userBody);
+        }
     }
 };
