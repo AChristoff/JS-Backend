e006618e3082278efbede7ee8c3fea54f9332792
AChristoff <aleksov101@gmail.com>
2020-05-31 22:35:39 +0300

    add validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,7 +6,7 @@ function validatePost(req, res) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
      res.status(422).json({
-      message: 'Validation failed, entered data is incorrect',
+      message: 'Feed data error!',
       errors: errors.array()
     });
 
