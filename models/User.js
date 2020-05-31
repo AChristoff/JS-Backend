71b88f56f6ff8b6e954f485f6b73c7b10cdc9e05
AChristoff <aleksov101@gmail.com>
2020-05-31 21:06:21 +0300

    Express API

--- models/User.js
+++ models/User.js
@@ -1,48 +1,35 @@
 const mongoose = require('mongoose');
 const encryption = require('../util/encryption');
+const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
-    username: {type: mongoose.Schema.Types.String, required: true, unique: true},
-    hashedPass: {type: mongoose.Schema.Types.String, required: true},
-    firstName: {type: mongoose.Schema.Types.String},
-    lastName: {type: mongoose.Schema.Types.String},
-    salt: {type: mongoose.Schema.Types.String, required: true},
-    roles: [{type: mongoose.Schema.Types.String}],
-    rents: [{type: mongoose.Schema.Types.ObjectId, ref: 'Rent'}],
+const userSchema = new Schema({
+  email: {
+    type: Schema.Types.String,
+    required: true
+  },
+  hashedPassword: {
+    type: Schema.Types.String,
+    required: true
+  },
+  name: {
+    type: Schema.Types.String,
+    required: true
+  },
+  salt: {
+    type: Schema.Types.String,
+    required: true
+  },
+  posts: [
+    { type: Schema.Types.ObjectId, ref: 'Post' }
+  ]
 });
 
 userSchema.method({
-    authenticate: function (password) {
-        return encryption.generateHashedPassword(this.salt, password) === this.hashedPass;
-    }
-});
+  authenticate: function (password) {
+    const currentHashedPass = encryption.generateHashedPassword(this.salt, password);
 
-const User = mongoose.model('User', userSchema);
+    return currentHashedPass === this.hashedPassword;
+  }
+})
 
-User.seedAdmin = async () => {
-    const users = await User.find();
-
-    try {
-
-        if (users.length > 0) {
-            return;
-        }
-
-        const salt = encryption.generateSalt();
-        const hashedPass = encryption.generateHashedPassword(salt, 'admin');
-
-        return User.create({
-            username: 'Admin',
-            hashedPass,
-            firstName: 'Pesho',
-            lastName: 'Georgiev',
-            salt,
-            roles: ['Admin'],
-            rents: []
-        });
-    } catch (e) {
-        console.error(e);
-    }
-
-};
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
