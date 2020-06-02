4494b0923f6b4af33801ae9399a95a8b9d63bc51
AChristoff <aleksov101@gmail.com>
2020-06-02 09:06:23 +0300

    edit user validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -85,74 +85,76 @@ module.exports = {
             })
     },
     edit: (req, res, next) => {
-        const {email, password, newEmail, newPassword, name} = req.body;
+        if (validateUser(req, res)) {
+            const {email, password, newEmail, newPassword, name} = req.body;
 
-        User.findOne({email: email})
-            .then((user) => {
-                if (!user) {
-                    const error = new Error('User not found!');
-                    error.statusCode = 401;
-                    throw error;
-                }
+            User.findOne({email: email})
+                .then((user) => {
+                    if (!user) {
+                        const error = new Error('User not found!');
+                        error.statusCode = 401;
+                        throw error;
+                    }
 
-                if (!user.authenticate(password)) {
-                    const error = new Error('Invalid password!');
-                    error.statusCode = 401;
-                    throw error;
-                }
+                    if (!user.authenticate(password)) {
+                        const error = new Error('Invalid password!');
+                        error.statusCode = 401;
+                        throw error;
+                    }
 
-                const salt = user.salt;
-                let newUserEmail;
-                let newHashedPassword;
-                let newName;
+                    const salt = user.salt;
+                    let newUserEmail;
+                    let newHashedPassword;
+                    let newName;
 
-                if(newEmail) {
-                    newUserEmail = newEmail;
-                } else {
-                    newUserEmail = email;
-                }
+                    if (newEmail) {
+                        newUserEmail = newEmail;
+                    } else {
+                        newUserEmail = email;
+                    }
 
-                if(newPassword) {
-                    newHashedPassword = encryption.generateHashedPassword(salt, newPassword);
-                } else {
-                    newHashedPassword = encryption.generateHashedPassword(salt, password);
-                }
+                    if (newPassword) {
+                        newHashedPassword = encryption.generateHashedPassword(salt, newPassword);
+                    } else {
+                        newHashedPassword = encryption.generateHashedPassword(salt, password);
+                    }
 
-                if(name) {
-                    newName = name;
-                } else {
-                    newName = user.name;
-                }
+                    if (name) {
+                        newName = name;
+                    } else {
+                        newName = user.name;
+                    }
 
-                if (!newEmail && !newPassword && !name) {
-                    const error = new Error('New feed is required');
-                    error.statusCode = 400;
-                    throw error;
-                }
+                    if (!newEmail && !newPassword && !name) {
+                        const error = new Error('New feed is required');
+                        error.statusCode = 400;
+                        throw error;
+                    }
 
-                const newUserData = {
-                    email: newUserEmail,
-                    hashedPassword: newHashedPassword,
-                    name: newName,
-                    salt,
-                };
+                    const newUserData = {
+                        email: newUserEmail,
+                        hashedPassword: newHashedPassword,
+                        name: newName,
+                        salt,
+                    };
 
-                return User.updateOne({email: email}, newUserData);
-            })
-            .then(() => {
+                    return User.updateOne({email: email}, newUserData);
+                })
+                .then(() => {
 
-                res.status(200).json(
-                    {
-                        message: 'User successfully edited!',
-                    });
-            })
-            .catch(error => {
-                if (!error.statusCode) {
-                    error.statusCode = 500;
-                }
+                    res.status(200).json(
+                        {
+                            message: 'User successfully edited!',
+                        });
+                })
+                .catch(error => {
+                    if (!error.statusCode) {
+                        error.statusCode = 500;
+                    }
 
-                next(error);
-            })
+                    next(error);
+                })
+        }
     },
     delete: (req, res, next) => {
         const {email, password} = req.body;
--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const regExp = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':
 router.post('/register',
     [
         body('email')
-            .isEmail().withMessage('Please enter a valid email.')
+            .isEmail().withMessage('Please enter a valid email!')
             .custom((value, {req}) => {
                 return User.findOne({email: value})
                     .then(userDoc => {
@@ -19,19 +19,51 @@ router.post('/register',
         body('password')
             .trim()
             .matches(regExp, 'i')
-            .withMessage('Password must be min 6 char long and contain at least one uppercase, lower case, special char and one digit.'),
+            .withMessage('Password must be min 6 char long and contain at least one uppercase, lower case, special char and one digit!'),
         body('name')
             .trim()
             .not()
-            .isEmpty().withMessage('Please enter a valid name.')
-            .matches(/^[A-Za-z\s]+$/).withMessage('Name must be alphabetic.')
+            .isEmpty().withMessage('Please enter a valid name!')
+            .matches(/^[A-Za-z\s]+$/).withMessage('Name must be alphabetic!')
             .escape()
     ],
     authController.register
 );
 
 router.post('/login', authController.login);
-router.post('/edit', authController.edit);
+router.post('/edit',
+    [
+        body('email')
+            .isEmail().withMessage('Please enter a valid email!'),
+        body('password')
+            .trim()
+            .matches(regExp, 'i')
+            .withMessage('Password must be min 6 char long and contain at least one uppercase, lower case, special char and one digit!'),
+        body('newEmail')
+            .if((value, {req}) => value !== '')
+            .isEmail().withMessage('Please enter a valid email!')
+            .custom((value, {req}) => {
+                return User.findOne({email: value})
+                    .then(userDoc => {
+                        if (userDoc) {
+                            return Promise.reject('E-Mail address already exists!');
+                        }
+                    })
+            }),
+        body('newPassword')
+            .if((value, {req}) => value !== '')
+            .trim()
+            .matches(regExp, 'i')
+            .withMessage('Password must be min 6 char long and contain at least one uppercase, lower case, special char and one digit!'),
+        body('name')
+            .if((value, {req}) => value !== '')
+            .trim()
+            .not()
+            .isEmpty().withMessage('Please enter a valid name!')
+            .matches(/^[A-Za-z\s]+$/).withMessage('Name must be alphabetic!')
+            .escape(),
+    ],
+    authController.edit);
 router.post('/delete', authController.delete);
 
 module.exports = router;
