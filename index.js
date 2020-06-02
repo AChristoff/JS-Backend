3d9f1c70da264d4312acaebea39fba102337cff6
AChristoff <aleksov101@gmail.com>
2020-06-03 01:20:17 +0300

    add config files

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
-
 const {connectionString, port} = require('./config/environment');
 const initializeDataBase = require('./config/database');
-
+const CORS = require('./config/cors');
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 const app = express();
 
 initializeDataBase(connectionString);
+CORS(app);
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
 
