10439d872d3fa879f1b36e8853269d71c2e6a93a
AChristoff <aleksov101@gmail.com>
2020-06-06 21:00:37 +0300

    reset branch

--- middleware/interceptor.js
+++
@@ -1,7 +0,0 @@
-const postRoutes = require('../routes/postRouts');
-const userRoutes = require('../routes/userRoutes');
-
-module.exports = (app) => {
-    app.use('/blog', postRoutes);
-    app.use('/user', userRoutes);
-};
